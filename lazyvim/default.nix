d9817f9a9f9eedf0683138669e26ba83bf4513c6
Armin Fröhlich <mail@arminfroehlich.ch>
2024-12-04 11:05:42 +0100

    fix catppuccin name ref

--- lazyvim/default.nix
+++ lazyvim/default.nix
@@ -135,6 +135,7 @@ in
               else
                 ""
             }
+        		{ "catppuccin/nvim", name = "catppuccin-nvim" },
         		{ "williamboman/mason-lspconfig.nvim", enabled = false },
         		{ "williamboman/mason.nvim", enabled = false },
         		{
